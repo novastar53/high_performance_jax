14f9dd1cab66cd410d3fb378f187919b6e4811e1
Vikram Pawar <pvikram035@gmail.com>
2025-05-23 11:25:41 -0400

    Fixed bug in hbm memory measurement.

--- src/high_performance_jax/single_chip_performance.py
+++ src/high_performance_jax/single_chip_performance.py
@@ -7,9 +7,9 @@ def measure_tpu_hbm_memory_transfer(op, dim, dtype):
 
     match op:
         case "matadd":
-            return 3 * dim * bytes 
+            return 3 * (dim**2) * bytes 
         case "matadd3":
-            return 6 * dim * bytes 
+            return 6 * (dim**2) * bytes 
         case "matmul":
             return 3 * (dim**2) * bytes
         case _:
@@ -18,9 +18,9 @@ def measure_tpu_hbm_memory_transfer(op, dim, dtype):
 def measure_tpu_flops(op, dim):
     match op:
         case "matadd":
-            return dim ** 2
+            return dim**2
         case "matadd3":
-            return dim ** 3
+            return dim**3
         case "matmul":
             return (dim**2)*(2*dim - 1)
         case _:
