423c12678d7b42b8ecbd27b2526ff9bc1e8a9d8b
Vikram Pawar <pvikram035@gmail.com>
2025-05-23 14:51:01 -0400

    Added bmm for saturating memory bandwidth.

--- src/high_performance_jax/single_chip_performance.py
+++ src/high_performance_jax/single_chip_performance.py
@@ -1,32 +1,32 @@
+import jax
 import jax.numpy as jnp
 
 
-
-def measure_tpu_hbm_memory_transfer(op, dim, dtype):
+def measure_tpu_hbm_memory_transfer(op, n, dim, dtype):
     bytes = jnp.dtype(dtype).itemsize
 
     match op:
         case "matadd":
-            return 3 * (dim**2) * bytes 
+            return n * 3 * (dim**2) * bytes 
         case "matadd3":
-            return 6 * (dim**2) * bytes 
+            return n * 6 * (dim**2) * bytes 
         case "matmul":
-            return 3 * (dim**2) * bytes
+            return n * 3 * (dim**2) * bytes
         case "matmul3":
-            return 6 * (dim**2) * bytes
+            return n * 6 * (dim**2) * bytes
         case _:
             return 0
 
-def measure_tpu_flops(op, dim):
+def measure_tpu_flops(op, n, dim):
     match op:
         case "matadd":
-            return dim**2
+            return n * dim**2
         case "matadd3":
-            return 2*dim**2
+            return n * 2*dim**2
         case "matmul":
-            return (dim**2)*(2*dim - 1)
+            return n * (dim**2)*(2*dim - 1)
         case "matmul3":
-            return 2*(dim**2)*(2*dim - 1)
+            return n * 2*(dim**2)*(2*dim - 1)
         case _:
             return 0
 
@@ -43,3 +43,5 @@ def matmul(A, B):
 def matmul3(A, B, C):
     return A @ B @ C
 
+def bmm(A, B):
+    return jax.lax.batch_matmul(A, B)
