8b956424ed404ce865c56022f530c75551c58a2d
Vikram Pawar <pvikram035@gmail.com>
2025-10-05 00:46:55 -0400

    Made progress on flash attention impl.

--- src/high_performance_jax/flash_attention.py
+++ src/high_performance_jax/flash_attention.py
@@ -1,6 +1,20 @@
 import jax
 import jax.numpy as jnp
 
+# online softmax
+x = jnp.array([1, 5, 2, 8])
+z = 0
+max_x = float("-inf")
+for i in range(len(x)):
+    new_max_x = max(x[0], max_x)
+    z = z*jnp.exp(max_x - new_max_x) + jnp.exp(x[i] - new_max_x)
+    max_x = new_max_x
+
+result = jnp.exp(x - max_x) / z
+standard_result = jax.nn.softmax(x)
+print(jnp.allclose(result, standard_result))
+
+
 def print_grid(N, filled_cells):
     """
     Prints an NxN grid.
@@ -23,17 +37,17 @@ def print_grid(N, filled_cells):
 
 N, D = 24, 8
 
-q = jax.random.normal(
+q = jax.random.randint(
     jax.random.key(0),
-    (N, D)
+    (N, D), -5, 5
 )
-k = jax.random.normal(
+k = jax.random.randint(
     jax.random.key(1),
-    (N, D)
+    (N, D), -5, 5
 )
-v = jax.random.normal(
+v = jax.random.randint(
     jax.random.key(2),
-    (N, D)
+    (N, D), -5, 5
 )
 
 # regular attention
@@ -42,17 +56,42 @@ a = jax.nn.softmax(a, axis=-1)
 o = jnp.einsum('nm,md->nd', a, v)
 print(o.shape)
 
+
 # flash attention (python)
 block_size = 4
 n_blocks = N // block_size
 
 computed_blocks = []
-result = jnp.empty((N, N))
+result = jnp.zeros_like(q, dtype=jnp.float32)
+z = jnp.zeros((N,), dtype=jnp.float32)
+m = jnp.full((N,), float('-inf'))
 for row in range(n_blocks):
+    q_blk = q[row*block_size:(row+1)*block_size, :]
+    m_blk = m[row*block_size:(row+1)*block_size]
+    z_blk = z[row*block_size:(row+1)*block_size]
+    result_blk = result[row*block_size:(row+1)*block_size, :]
     for col in range(n_blocks):
-        q_blk = q[row*block_size:(row+1)*block_size, :]
         k_blk = k.T[:, col*block_size:(col+1)*block_size] 
-        result = result.at[row*block_size:(row+1)*block_size, col*block_size:(col+1)*block_size].set(q_blk @ k_blk)
+        v_blk = v[col*block_size:(col+1)*block_size, :]
+        s_blk = q_blk @ k_blk
+        m_blk_old = m_blk
+        m_blk = jnp.maximum(m_blk, jnp.max(s_blk, axis=-1))
+        p_blk = jnp.exp(s_blk - m_blk)
+        z_blk = z_blk*jnp.exp(m_blk_old - m_blk) + jnp.sum(p_blk, axis=-1)
+        p = p_blk @ v_blk
+        result_blk = (jnp.eye(block_size,) * jnp.exp(m_blk - m_blk_old)) @ result_blk + p
         computed_blocks.append((row, col))
-        print_grid(n_blocks, computed_blocks)
-        print("-----")
+        #print_grid(n_blocks, computed_blocks)
+        #print("-----")
+    m = m.at[row*block_size:(row+1)*block_size].set(m_blk)
+    z = z.at[row*block_size:(row+1)*block_size].set(z_blk)
+    result = result.at[row*block_size:(row+1)*block_size, :].set(result_blk)
+
+#result = jnp.exp(result - jnp.eye(N, N) * jnp.max(result, axis=-1))
+#z = jnp.sum(result, axis=-1)
+result /= z[..., None]
+
+s = q @ k.T
+p = jax.nn.softmax(s, axis=-1)
+test_result = p @ v
+print(jnp.allclose(result, test_result))
