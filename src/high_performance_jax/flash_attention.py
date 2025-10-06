24dfdf2ba1796f4cc3cb3761fec45daa6c054bdb
Vikram Pawar <pvikram035@gmail.com>
2025-10-06 13:44:56 -0400

    Started working on triton flash attention implementation.

--- src/high_performance_jax/flash_attention.py
+++ src/high_performance_jax/flash_attention.py
@@ -105,8 +105,98 @@ result /= z[..., None]
 assert(jnp.allclose(result, manual_result))
 
 
+
 # flash attention (triton)
-# TODO
+import torch
+import triton
+import triton.language as tl
+
+class TritonAttention(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, Q, K, V, causal, dtype=torch.float16):
+        HEAD_DIM_Q, HEAD_DIM_K, HEAD_DIM_V  = Q.shape[-1], K.shape[-1], V.shape[-1]
+        BATCH_SIZE, NUM_HEADS, SEQ_LEN, HEAD_DIM = Q.shape
+
+        assert HEAD_DIM_Q == HEAD_DIM_K and HEAD_DIM_K == HEAD_DIM_V
+
+        O = torch.empty_like(Q)
+        stage = 3 if causal else 1
+
+        grid = lambda args: (
+            triton.cdiv(SEQ_LEN, args["BLOCK_SIZE_Q"]),
+            BATCH_SIZE * NUM_HEADS,
+            1,
+        )
+
+
+def test_op(BATCH_SIZE, NUM_HEADS, SEQ_LEN, HEAD_DIM, causal, dtype=torch.float16):
+    Q = (
+        torch.empty(
+        (BATCH_SIZE, NUM_HEADS, SEQ_LEN, HEAD_DIM), dtype=dtype, device='cuda'
+        ) 
+        .normal_(mean=0.0, std=0.5) 
+        .requires_grad_()
+    )
+
+    K = (
+        torch.empty(
+        (BATCH_SIZE, NUM_HEADS, SEQ_LEN, HEAD_DIM), dtype=dtype, device='cuda'
+        ) 
+        .normal_(mean=0.0, std=0.5) 
+        .requires_grad_()
+    )
+
+    V = (
+        torch.empty(
+        (BATCH_SIZE, NUM_HEADS, SEQ_LEN, HEAD_DIM), dtype=dtype, device='cuda'
+        ) 
+        .normal_(mean=0.0, std=0.5) 
+        .requires_grad_()
+    )
+
+    softmax_scale = 1 / (HEAD_DIM**0.5)
+    dO = torch.randn_like(Q)
+
+
+
+    # reference attention (torch)
+    MASK = torch.tril(torch.ones((SEQ_LEN, SEQ_LEN), device='cuda'))
+    P = torch.matmul(Q, K.transpose(2, 3)) * softmax_scale
+    if causal:
+        P[:, :, MASK == 0] = float("-inf")
+    P = torch.softmax(P.float(), dim=-1).half()
+    ref_O = torch.matmul(P, V)
+    ref_O.backward(dO)
+    ref_dV, V.grad = V.grad.clone(), None
+    ref_dK, K.grad = K.grad.clone(), None
+    ref_dQ, Q.grad = Q.grad.clone(), None
+
+
+    # triton implementation
+    tri_out = TritonAttention.apply(Q, K, V, causal, softmax_scale).half()
+    tri_out.backward()
+    tri_dV, V.grad = V.grad.clone(), None
+    tri_dK, K.grad = K.grad.clone(), None
+    tri_dQ, Q.grad = Q.grad.clone(), None
+
+    # compare
+    rtol = 0.0
+    atol = 1e-2
+    assert torch.allclose(ref_O, tri_out, atol=atol, rtol=rtol)
+    assert torch.allclose(ref_dK, tri_dK, atol=atol, rtol=rtol)
+    assert torch.allclose(ref_dV, tri_dV, atol=atol, rtol=rtol)
+
+
+    
+
+
+
+
+
+
+
+
 
 
 # flash attention (pallas)
