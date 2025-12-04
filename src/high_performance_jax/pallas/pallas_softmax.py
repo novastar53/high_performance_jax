3f93801a25f1c05a1060f6b1a62943e608d5a840
Vikram Pawar <pvikram035@gmail.com>
2025-12-03 23:50:34 -0500

    Continuing to work on pallas softmax.

--- src/high_performance_jax/pallas/pallas_softmax.py
+++ src/high_performance_jax/pallas/pallas_softmax.py
@@ -24,24 +24,29 @@ assert(jnp.allclose(probs_ref, probs_manual))
 # Pallas softmax
 G = 8 # Num groups
 
-def softmax_kernel(x_ref, o_ref):
+def softmax_kernel(x_ref, mi_ref, o_ref, mo_ref):
+    print(mi_ref.shape)
     x_reg = x_ref[...]
     o_reg = jnp.exp(x_reg)
     o_ref[...] = o_reg
+    mo_ref[...] = mi_ref
 
 
 @jax.jit
-def softmax(logits):
+def softmax(logits, m):
     result = pl.pallas_call(
         softmax_kernel,
-        out_shape=jax.ShapeDtypeStruct(logits.shape, logits.dtype),
+        out_shape=[jax.ShapeDtypeStruct(logits.shape, logits.dtype), jax.ShapeDtypeStruct(m.shape, m.dtype)],
         grid=(G, G),
-        in_specs=[pl.BlockSpec((logits.shape[0] // G, logits.shape[1] // G), lambda i, j: (i, j))],
-        out_specs=pl.BlockSpec((logits.shape[0] // G, logits.shape[1] // G), lambda i, j: (i, j))
-    )(logits)
+        in_specs=[pl.BlockSpec((logits.shape[0] // G, logits.shape[1] // G), lambda i, j: (i, j)),
+                  pl.BlockSpec(m.shape, lambda i, j: (i, 0))],
+        out_specs=[pl.BlockSpec((logits.shape[0] // G, logits.shape[1] // G), lambda i, j: (i, j)),
+                   pl.BlockSpec(m.shape, lambda i, j: (i, 0))]
+    )(logits, m)
     return result
 
 
-probs_pl = softmax(logits)
+m = jnp.ones((logits.shape[0], 1)) * float('inf') * (-1)
+probs_pl, m_pl = softmax(logits, m)
 s = jnp.exp(logits)
 assert(jnp.allclose(probs_pl, s))
